611c3bce30e2c8c6b37d6661fc1802661f7d6c0a
Vinicius <mitnewzealand@gmail.com>
2019-12-04 21:30:07 -0300

    initial commit

---
+++ src/main/java/com/invillia/thundercoin/ThundercoinApplication.java
@@ -0,0 +1,13 @@
+package com.invillia.thundercoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThundercoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThundercoinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/invillia/thundercoin/ThundercoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invillia.thundercoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThundercoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
